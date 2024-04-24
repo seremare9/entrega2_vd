7e2c7f4c6d0fb99a489ca4bb7628e69453693a38
seremare9 <sere6819@gmail.com>
2024-04-24 11:13:39 -0300

    Cambios

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
   let Genero = d3
     .scaleOrdinal() /* dato categóricos */
     .domain(["Femenino", "Masculino", "No Binario"])
-    .range(["../images/CORCHEA.svg", "../images/semicorchea.svg", "../images/negra.svg"])
+    // .range(["../images/CORCHEA.svg", "../images/semicorchea.svg", "../images/negra.svg"])
 
   /* Escala para tiempo de escucha (tamaño de la nota musical)*/
   let TiempoEnHoras = d3
@@ -47,11 +47,10 @@
 <main>
   <div class="header">
     <img src="/images/logo.png" width="100" alt="logo" />
-    <h3 class="headline">
-      <b>Preferencias Musicales</b>
-      Visualización de Datos
-    </h3>
-    <p class="bajada">Josefina Jahde y Serena Marelli</p>
+    <h3 class="titulo">Preferencias Musicales</h3>
+    <h2 class="subtitulo">Representación visual</h2>
+      
+    <!-- <p class="bajada">Josefina Jahde y Serena Marelli</p> -->
   </div>
 
   <!-- Conedor de las entidades -->
@@ -77,11 +76,6 @@
           stroke="{MomentoDeEscucha(rta.Momento)}" stroke-width="4" stroke-miterlimit="10"/>
           </svg>
           
-        <!-- <svg id="Capa_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82.96 121.85"><defs><style>.cls-1{fill:#fff;stroke-width:0px;}</style></defs><g id="Capa_1-2"><path class="cls-1" d="M43.7,105c.78-3.03,1.71-8.32,1-16v-53c3.9.77,10.62,2.69,17,8,9.68,8.06,12.24,18.64,13,22,.78,3.42.95,6.36,1,8,.18,6.47-1.19,11.5-2,14-.7,2.14-1.45,3.85-2,5-.11.37-.21.84,0,1,.39.3,1.6-.68,2-1,2.72-2.19,7.5-10.84,9-22,.21-1.59.46-4,0-7-.28-1.78-.7-3.12-1-4-1.26-3.73-5.73-16.98-14.19-26.85-.5-.58-1.53-1.79-2.81-3.15-.08-.08.03.03-7-7-5.55-5.55-8.39-8.43-11-13-.94-1.64-1.59-3.06-2-4V0h-6v88c-3.31-.5-8.21-.89-14,0-4.93.76-10.81,1.67-16,6-4.54,3.79-10.81,12.08-8,19,2.18,5.35,8.65,7.1,12,8,11.25,3.04,20.53-3.01,22-4,1.94-1.31,5.83-4.01,8-9,.34-.77.65-1.64,1-3Z"/></g></svg> -->
-            <!-- <svg width="{TiempoEnHoras(rta.TiempoEnHoras)}" viewBox="0 0 82.25 121.83" xmlns="http://www.w3.org/2000/svg"> 
-              <path d="M43.7,105c.78-3.03,1.71-8.32,1-16v-53c3.9.77,10.62,2.69,17,8,9.68,8.06,12.24,18.64,13,22,.78,3.42.95,6.36,1,8,.18,6.47-1.19,11.5-2,14-.7,2.14-1.45,3.85-2,5-.11.37-.21.84,0,1,.39.3,1.6-.68,2-1,2.72-2.19,7.5-10.84,9-22,.21-1.59.46-4,0-7-.28-1.78-.7-3.12-1-4-1.26-3.73-5.73-16.98-14.19-26.85-.5-.58-1.53-1.79-2.81-3.15-.08-.08.03.03-7-7-5.55-5.55-8.39-8.43-11-13-.94-1.64-1.59-3.06-2-4V0h-6v88c-3.31-.5-8.21-.89-14,0-4.93.76-10.81,1.67-16,6-4.54,3.79-10.81,12.08-8,19,2.18,5.35,8.65,7.1,12,8,11.25,3.04,20.53-3.01,22-4,1.94-1.31,5.83-4.01,8-9,.34-.77.65-1.64,1-3Z"
-              fill="{MomentoDeEscucha(rta.Momento)}"/> -->
-            <!-- </svg> -->
         {:else if rta.Genero == "No Binario"}
         <svg width="{TiempoEnHoras(rta.TiempoEnHoras)}" viewBox="0 0 45 122" fill="{MomentoDeEscucha(rta.Momento)}" xmlns="http://www.w3.org/2000/svg">
           <path d="M41.4101 0C41.3001 58.76 41.3201 82.96 41.4101 88C41.4101 88.08 41.4401 89.75 41.4101 92C41.3201 98.8 41.2701 102.2 40.4101 105C38.6101 110.88 33.5901 114.22 32.4101 115C22.4801 121.6 6.10006 119.04 3.41006 112C1.38006 106.69 7.56006 99.93 8.41006 99C19.3101 87.07 39.2601 91.49 41.4101 92" fill="{MomentoDeEscucha(rta.Momento)}"/>
@@ -90,10 +84,20 @@
           </svg>
         {/if}
       </div>
-        <svg height="300" width="300" xmlns="http://www.w3.org/2000/svg" class="difuminado">
-          <circle r="110" cx="150" cy="150" fill={GeneroMusical(rta.GeneroMusical)}/>
+
+        {#if rta.Plataforma == "Spotify"}
+          <svg height="300" width="300" xmlns="http://www.w3.org/2000/svg" class="difuminado">
+            <circle r="110" cx="150" cy="150" fill={GeneroMusical(rta.GeneroMusical)}/>
+          </svg>
+
+        {:else if rta.Plataforma == "Tidal"}
+          <svg height="300" width="300" xmlns="http://www.w3.org/2000/svg" class="difuminado">
+            <rect width="200" height="200" x="10" y="10" rx="20" ry="20" fill={GeneroMusical(rta.GeneroMusical)}/>
+          </svg>
+
+        {/if}
              <!-- Falta definir la forma de Tidal -->
-        </svg>
+        
         <!-- <img src="{Plataforma(rta.Plataforma)}" alt="sombra"> -->
         <!-- style="width: {TiempoEnHoras(rta.TiempoDeEscucha)}px;"  -->
         <p class="name">
@@ -120,21 +124,25 @@
     margin-top: 50px;
     margin-bottom: 80px;
   }
-  .headline {
+  .titulo {
+    font-size: 45px;
+    line-height: 1.2;
+    font-weight: bold;
+    text-align: center;
+    margin: 10px;
+  }
+  .subtitulo {
     font-size: 30px;
     line-height: 1.2;
     font-weight: normal;
     text-align: center;
-    margin: 20px;
+    margin: 10px;
   }
   .bajada {
     font-size: 18px;
     font-weight: normal;
     text-align: center;
     margin: 10px;
-  }
-  .headline b {
-    display: block;
   }
   .container {
     display: flex;
@@ -152,33 +160,39 @@
     flex-direction: column;
     align-items: center;
     flex: 180px 0 0;
+    transition: 0.5;
   }
+
+  /* .person-container:hover {
+    scale:
+    
+  } */
 
   .aspectoNota {
     z-index: 2;
   }
   .name {
-    font-size: 15px;
+    font-size: 20px;
     color: rgb(65, 65, 65);
     font-weight: bold;
     text-align: center;
     margin-top: 5px;
   }
   .cancionFav {
-    font-size: 13px;
+    font-size: 15px;
     color: rgb(65, 65, 65);
     font-weight: light;
     font-style: italic;
     text-align: center;
-    margin-top: 5px;
+    /* margin-top: 5px; */
   }
   
   .difuminado{
     background: radial-gradient(circle, transparent 0%, rgba(255,255,255,0.5) 100%); /* Crea un gradiente radial */
+    background-color:aliceblue;
     filter: blur(9px);
     z-index: 1;
   }
 
-  
     
 </style>
